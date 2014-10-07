49583c18053d4187a1ea3cb1f5592f6ceb144926
dudrpdjwls <dudrpdjwls@efecd4e3-bc03-0510-bdcf-e5a0e64816a0>
2014-10-07 16:42:24 +0000

    Timeattack 수정

--- TimeAttack/src/com/fivetrue/timeattack/fragment/DrawerFragment.java
+++ TimeAttack/src/com/fivetrue/timeattack/fragment/DrawerFragment.java
@@ -6,7 +6,9 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class DrawerFragment extends Fragment {
@@ -16,6 +18,14 @@ public class DrawerFragment extends Fragment {
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.fragment_drawer, null);
+		view.findViewById(R.id.btn_map).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "Click Button", Toast.LENGTH_SHORT).show();
+			}
+		});
 		return view;
 	}
 }
