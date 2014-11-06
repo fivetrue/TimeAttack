6e77466963c0b72c5b203c2b4ddbaf687996c021
dudrpdjwls <dudrpdjwls@efecd4e3-bc03-0510-bdcf-e5a0e64816a0>
2014-11-06 06:29:27 +0000

    SearchLocation Activity 추가.

---
+++ TimeAttack/src/com/fivetrue/timeattack/activity/SearchLocationActivity.java
@@ -0,0 +1,73 @@
+package com.fivetrue.timeattack.activity;
+
+import com.fivetrue.timeattack.R;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class SearchLocationActivity extends BaseActivity {
+
+	@Override
+	View onCreateView(LayoutInflater inflater) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	String getActionBarTitleName() {
+		// TODO Auto-generated method stub
+		return getString(R.string.actionbar_search);
+	}
+
+	@Override
+	String getActionBarSubTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getActionBarMenuResource() {
+		// TODO Auto-generated method stub
+		return INVALID_VALUE;
+	}
+
+	@Override
+	boolean isHomeAsUp() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	void requsetNetworkResultSuccess() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void outOfService(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void avaliableService(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void temporarilyServiceUnavailable(String provider, int status,
+			Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void changeLocation(Location location) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
