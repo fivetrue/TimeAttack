c1e71bf5b23d71d7119a1b353eecca204bd7ef70
dudrpdjwls <dudrpdjwls@efecd4e3-bc03-0510-bdcf-e5a0e64816a0>
2014-11-10 03:02:53 +0000

    Pager Setting

--- src/com/fivetrue/timeattack/view/pager/CustomViewPager.java
+++ src/com/fivetrue/timeattack/view/pager/CustomViewPager.java
@@ -1,13 +1,61 @@
 package com.fivetrue.timeattack.view.pager;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
 
 public class CustomViewPager extends ViewPager {
+	public interface OnSwipeDirectionListener{
+		public void onSwipeLeft(MotionEvent event);
+		public void onSwipeRight(MotionEvent event);
+		public void onSwipeComplete(MotionEvent event);
+	}
 
+	private OnSwipeDirectionListener mOnSwipeDirectionListener = null;
+	
+	private boolean isTouch = false;
+	private Point mLastTouchPoint = new Point();
+	
 	public CustomViewPager(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if(mOnSwipeDirectionListener == null){
+			return super.onTouchEvent(event);
+		}
+		switch(event.getAction()){
+		case MotionEvent.ACTION_DOWN :
+			isTouch = true;
+			mLastTouchPoint.x = (int) event.getX();
+			mLastTouchPoint.y = (int) event.getY();
+			break;
+			
+		case MotionEvent.ACTION_MOVE :
+			
+			if(isTouch){
+				if(mLastTouchPoint.x < event.getX()){
+					mOnSwipeDirectionListener.onSwipeRight(event);
+				}else{
+					mOnSwipeDirectionListener.onSwipeLeft(event);
+				}
+			}
+			break;
+			
+		case MotionEvent.ACTION_UP :
+			mOnSwipeDirectionListener.onSwipeComplete(event);
+			isTouch = false;
+			break;
+		}
+		
+		return super.onTouchEvent(event);
+	}
+	
+	public void setOnSwipeDirectionListener(OnSwipeDirectionListener listener){
+		mOnSwipeDirectionListener = listener;
+	}
 }
