4fb2969a551ffa57df8c747212912e6a4a38bb8e
dudrpdjwls <dudrpdjwls@efecd4e3-bc03-0510-bdcf-e5a0e64816a0>
2014-11-11 10:02:43 +0000

    Actionbar Color Change.

--- src/com/fivetrue/timeattack/activity/BaseActivity.java
+++ src/com/fivetrue/timeattack/activity/BaseActivity.java
@@ -24,6 +24,7 @@ import com.fivetrue.timeattack.fragment.DrawerFragment.OnDrawerMenuClickListener
 import com.fivetrue.timeattack.view.HomeButtonView;
 import com.fivetrue.utils.Logger;
 
+import android.R.integer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -46,19 +47,20 @@ import android.widget.Toast;
  * @author Fivetrue
  *
  */
-	
+
 abstract public class BaseActivity extends LocationActivity implements IRequestResult{
-	
+
 	class ActionBarLayout{
 		protected ViewGroup mActionBar = null;
 		protected ViewGroup mActionBarLayout = null;
+		protected ViewGroup mActionBarHomeButtonGroup = null;
 		protected ViewGroup mActionBarHomeViewGroup = null;
-		
+
 		protected TextView mTvHomeTitle = null;
 		protected TextView mTvHomeSubtitle = null;
 		protected HomeButtonView mIvHomeButton = null;
 		protected View mShadow = null;
-		
+
 		public ViewGroup getActionBarLayout() {
 			return mActionBarLayout;
 		}
@@ -80,7 +82,14 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		public HomeButtonView getHomeButton() {
 			return mIvHomeButton;
 		}
+		public ViewGroup getActionBarHomeButtonGroup(){
+			return mActionBarHomeButtonGroup;
+		}
 	}
+	
+	protected int[] PRIMARY_COLOR = {0xF, 0x4, 0x4, 0x3, 0x3, 0x6};//0xFFF44336
+	protected int[] PRIMARY_DARK_COLOR = {0xB, 0x7, 0x1, 0xC, 0x1, 0xC};//0xFFB71C1C;
+	
 	protected boolean isRunningGps = false;
 	protected final int INVALID_VALUE = -1;
 	private DrawerLayout mDrawerLayout = null;
@@ -89,24 +98,24 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 	private ViewGroup mLayoutDrawer = null;
 	static protected DrawerFragment mFragmentDrawer = null;
 	private NetworkResultDBManager mNetworkResultDBM = null;
-	
+
 	private ActionBarLayout mActionbarLayout = new ActionBarLayout();
-	
+
 	private LayoutInflater mInflater = null;
-	
+
 	@Override
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_base);
 		mInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		
+
 		initActionBar(mInflater);
 		initViews(mInflater);
 		initActionBarSetting();
 		initModels();
 	}
-	
+
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
@@ -115,7 +124,7 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			onStartLocationService();
 		}
 	}
-	
+
 	@Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
@@ -127,42 +136,45 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			isRunningGps = false;
 		}
 	}
-	
+
 	public void initActionBar(LayoutInflater inflater){
 		mActionbarLayout.mActionBar = (ViewGroup) inflater.inflate(R.layout.layout_action_bar, null);
 		mActionbarLayout.mActionBarLayout = (ViewGroup) mActionbarLayout.mActionBar.findViewById(R.id.layout_actionbar);
 		mActionbarLayout.mActionBarHomeViewGroup = (ViewGroup) mActionbarLayout.mActionBar.findViewById(R.id.layout_actionbar_home);
+		mActionbarLayout.mActionBarHomeButtonGroup = (ViewGroup) mActionbarLayout.mActionBar.findViewById(R.id.layout_actionbar_home_icon);
 		mActionbarLayout.mIvHomeButton = (HomeButtonView) mActionbarLayout.mActionBar.findViewById(R.id.home_button) ;
 		mActionbarLayout.mTvHomeTitle = (TextView) mActionbarLayout.mActionBar.findViewById(R.id.tv_actionbar_home_title);
 		mActionbarLayout.mTvHomeSubtitle = (TextView) mActionbarLayout.mActionBar.findViewById(R.id.tv_actionbar_home_subtitle);
 		mActionbarLayout.mShadow = mActionbarLayout.mActionBar.findViewById(R.id.actionbar_shadow);
-		
+
+		mActionbarLayout.mActionBarHomeButtonGroup.setOnClickListener(onClickActionBarItem);
+
 		int shadowVisible = isActionBarBlending() ? View.GONE : View.VISIBLE;
 		mActionbarLayout.mShadow.setVisibility(shadowVisible);
 	}
-	
-	
+
+
 	/**
 	 * 기본적인 View를 초기화함.
 	 */
 	private void initViews(LayoutInflater inflater){
-		
+
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mContentView = (ViewGroup) findViewById(R.id.layout_main_frame);
 		mLayoutDrawer = (ViewGroup) findViewById(R.id.layout_drawer);
-		
+
 		if(mFragmentDrawer == null){
 			mFragmentDrawer = (DrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
 			mFragmentDrawer.setOnClickDrawerMenuClickListener(mDrawerMenuClickListener);
 		}
-		
+
 		View contentView = onCreateView(inflater);
 		if(contentView != null){
 			mContentView.addView(contentView);
 		}
-		
+
 		ViewGroup actionbar = null;
-		
+
 		if(isActionBarBlending()){
 			actionbar =  (ViewGroup) findViewById(R.id.layout_main);
 		}else{
@@ -176,15 +188,15 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			}
 		}
 	}
-	
+
 	/**
 	 * Actionbar Setting Area Start
 	 */
 	public void initActionBarSetting(){
-		
+
 		if(mActionbarLayout.getHomeButton() == null)
 			return ;
-		
+
 		mActionbarLayout.getHomeButton().setHomeAsUp(isHomeAsUp());
 		mActionbarLayout.getHomeTitle().setText(getActionBarTitleName());
 		if(!TextUtils.isEmpty(getActionBarSubTitle())){
@@ -193,8 +205,8 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		}
 		mDrawerLayout.setDrawerListener(onDrawerListener);
 	}
-	
-	
+
+
 	private void initActionBarButtons(Menu menu){
 		if(menu != null){
 			for(int i = 0 ; i < menu.size() ; i ++){
@@ -205,7 +217,7 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			}
 		}
 	}
-	
+
 	DrawerListener onDrawerListener = new DrawerListener() {
 
 		@Override
@@ -217,6 +229,7 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		public void onDrawerSlide(View arg0, float arg1) {
 			// TODO Auto-generated method stub
 			mActionbarLayout.getHomeButton().setAnimaitonValue(arg1);
+			changeColor(mActionbarLayout.getActionBarLayout(), PRIMARY_COLOR, PRIMARY_DARK_COLOR, arg1 * 0xF);
 		}
 
 		@Override
@@ -237,14 +250,50 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		}
 	};
 	
+	public void changeColor(View view, final int[] arrColorFrom, final int[] arrColorTo, float value){
+		if(view == null || arrColorFrom == null || arrColorTo == null
+				|| arrColorFrom.length <= 0 || arrColorTo.length <= 0 )
+			return;
+			
+		System.out.println("ojkwon : value = " + value);
+		int color[] = new int[arrColorFrom.length];
+		
+		for(int i = 0 ; i < arrColorFrom.length ; i ++){
+			if(arrColorFrom[i] > arrColorTo[i]){
+				float val = arrColorFrom[i] - value;
+				if(arrColorTo[i] <= val && val >= 0xF){
+					color[i] = (int)val;
+				}else{
+					color[i] = arrColorTo[i];
+				}
+			}else{
+				float val = arrColorFrom[i] - value;
+				if(arrColorTo[i] >= val && val >= 0){
+					color[i] = (int)val;
+				}else{
+					color[i] = arrColorTo[i];
+				}
+			}
+		}
+		String backround = new String();
+		backround += Integer.toHexString(15);
+		backround += Integer.toHexString(15);
+		for(int val : color){
+			backround += Integer.toHexString(val);
+		}
+		
+		long setColor = Long.valueOf(backround, 16);
+		view.setBackgroundColor((int)setColor);
+	}
+
 	/**
 	 * Actionbar Setting Area End
 	 */
-	
+
 	private void initModels(){
 		mNetworkResultDBM = new NetworkResultDBManager(getApplicationContext());
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -253,48 +302,23 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			inflater.inflate(getActionBarMenuResource(), menu);
 			initActionBarButtons(menu);
 		}
-		
+
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
-		if(onSelectedActionBarItem(item)){
-			return false;
-		}else{
-			return super.onOptionsItemSelected(item);
-		}
+
+		return super.onOptionsItemSelected(item);
 	}
-	
-	protected boolean onSelectedActionBarItem(MenuItem item){
-		int id = item.getItemId();
-		
-		switch(id){
-		case android.R.id.home :
-			if(isHomeAsUp()){
-//				NavUtils.navigateUpFromSameTask(this);
-				onBackPressed();
-			}else{
-				if(mDrawerLayout.isDrawerOpen(mLayoutDrawer)){
-					mDrawerLayout.closeDrawer(mLayoutDrawer);
-				}else{
-					mDrawerLayout.openDrawer(mLayoutDrawer);
-				}
-			}
-			return true;
-		}
-		
-		return false;
-	}
-	
-//	public ViewGroup getActionBarView() {
-//	    Window window = getWindow();
-//	    View v = window.getDecorView();
-//	    int resId = getResources().getIdentifier("action_bar_container", "id", "android");
-//	    return (ViewGroup) v.findViewById(resId);
-//	}
-	
+
+	//	public ViewGroup getActionBarView() {
+	//	    Window window = getWindow();
+	//	    View v = window.getDecorView();
+	//	    int resId = getResources().getIdentifier("action_bar_container", "id", "android");
+	//	    return (ViewGroup) v.findViewById(resId);
+	//	}
+
 	public void removeFragment(Fragment f, int startAni, int endAni){
 		FragmentTransaction trans =  getSupportFragmentManager().beginTransaction();
 		if(startAni != INVALID_VALUE && endAni != INVALID_VALUE){
@@ -303,9 +327,9 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		trans.remove(f);
 		trans.commit();
 	}
-	
-	
-	
+
+
+
 	public void hideFragment(Fragment f,  int startAni, int endAni){
 		FragmentTransaction trans =  getSupportFragmentManager().beginTransaction();
 		if(startAni != INVALID_VALUE && endAni != INVALID_VALUE){
@@ -314,30 +338,30 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		trans.hide(f);
 		trans.commit();
 	}
-	
+
 	public Fragment createFragment(Class<?> fragmentClass, String tag, Bundle argument){
 		return createFragment(INVALID_VALUE, fragmentClass, tag, INVALID_VALUE, argument , INVALID_VALUE, INVALID_VALUE);
 	}
-	
+
 	public Fragment createFragment(int containLayoutId, Class<?> fragmentClass, String tag, Bundle argument){
 		return createFragment(containLayoutId, fragmentClass, tag, INVALID_VALUE, argument , INVALID_VALUE, INVALID_VALUE);
 	}
-	
+
 	public Fragment createFragment(Class<?> fragmentClass, String tag, int transit, Bundle argument){
 		return createFragment(INVALID_VALUE, fragmentClass, tag, transit, argument , INVALID_VALUE, INVALID_VALUE);
 	}
-	
+
 	public Fragment createFragment(Class<?> fragmentClass, String tag, int transit, Bundle argument, int startAni, int endAni){
 		return createFragment(INVALID_VALUE, fragmentClass, tag, transit, argument , startAni, endAni);
 	}
-	
+
 	public Fragment createFragment(int containLayoutId, Class<?> fragmentClass
 			, String tag , int transit, Bundle argument, int aniStart, int aniEnd){
-		
+
 		if(containLayoutId == INVALID_VALUE){
 			containLayoutId  = R.id.layout_main_frame;
 		}
-		
+
 		if(transit == INVALID_VALUE){
 			transit = FragmentTransaction.TRANSIT_NONE;
 		}
@@ -367,26 +391,26 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		}
 		return null;
 	}
-	
-	
+
+
 	abstract View onCreateView(LayoutInflater inflater);
-	
+
 	abstract String getActionBarTitleName();
-	
+
 	abstract String getActionBarSubTitle();
-	
+
 	abstract int getActionBarMenuResource();
-	
+
 	abstract boolean isHomeAsUp();
-	
+
 	abstract void requsetNetworkResultSuccess();
-	
+
 	abstract void onClickAcitionMenuLocationSearch(View view);
-	
+
 	abstract boolean isActionBarBlending();
 	
 	protected OnDrawerMenuClickListener  mDrawerMenuClickListener = new OnDrawerMenuClickListener() {
-		
+
 		@Override
 		public void onMenuClick(ViewGroup parent, ViewGroup itemLayout,
 				TextView itemText) {
@@ -406,34 +430,52 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 					}
 				}
 			}
-			
+
 			if(itemText == null && itemLayout == null){
 				Log.e(getPackageName(), "Drawer layout item is null");
 				return;
 			}
-			
+
 			itemText.setSelected(true);
-			
+
 			if(mDrawerLayout != null && mLayoutDrawer != null){
 				mDrawerLayout.closeDrawer(mLayoutDrawer);
 			}
-			
+
 		}
 	};
-	
+
 	protected OnClickListener onClickActionBarItem = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			switch(v.getId()){
+
+			case R.id.layout_actionbar_home_icon :
+			{
+				if(mDrawerLayout == null || mLayoutDrawer == null)
+					return ;
+
+				if(isHomeAsUp()){
+					onBackPressed();
+				}else{
+					if(mDrawerLayout.isDrawerOpen(mLayoutDrawer)){
+						mDrawerLayout.closeDrawer(mLayoutDrawer);
+					}else{
+						mDrawerLayout.openDrawer(mLayoutDrawer);
+					}
+				}
+				return;
+			}
+
 			case R.id.action_item_setting :
 			{
 				//설정.
 				Toast.makeText(getApplicationContext(), "action_settings", Toast.LENGTH_SHORT).show();
 				return ;
 			}
-				
+
 
 			case R.id.action_item_place :
 			{
@@ -441,7 +483,7 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 				Toast.makeText(getApplicationContext(), "action_place", Toast.LENGTH_SHORT).show();
 				return ;
 			}
-			
+
 			case R.id.action_item_searching :
 			{
 				//위치 검색.
@@ -454,14 +496,14 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 				onClickAcitionMenuLocationSearch(v);
 				return;
 			}
-				
+
 			case R.id.action_item_map : 
 			{
 				//지도 이동.
 				startActivity(MapActivity.class);
 				return ;
 			}
-			
+
 			case R.id.action_item_alarm :
 			{
 				//알람.
@@ -480,11 +522,11 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			}
 		}
 	};
-	
+
 	public ActionBarLayout getActionBarLayout(){
 		return mActionbarLayout;
 	}
-	
+
 	public void onSuccessRequest(String url, org.json.JSONObject request) {
 		boolean isResultOK = false;
 		if(url.contains(Constants.GoogleDirectionsAPI.DIRECTION_API_HOST)){
@@ -503,7 +545,7 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 				isResultOK = true;
 			}
 		}else if(url.contains(SeoulAPIConstants.API_HOST)){
-			
+
 			if(url.contains(SeoulAPIConstants.Subway.ARRIVAL_INFO_SERVICE)){
 				SubwayArrivalInfoEntry entry = new SubwayArrivalInfoConverter().onReceive(request);
 				if(entry.getStatus().equals(SeoulAPIConstants.ResultInfo.OK)){
@@ -521,24 +563,24 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 			requsetNetworkResultSuccess();
 		}
 	};
-	
+
 	@Override
 	public void onFailRequest(String url) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	protected void log(String msg){
 		Logger.e(getPackageName(), getClass().getName() + " = " + msg);
 	}
-	
+
 	protected void startActivity(Class<?> cls){
 		startActivity(cls, false, null);
 	}
-	
+
 	protected void startActivity(Class<?> cls , boolean bringToTop){
 		startActivity(cls, bringToTop, null);
 	}
-	
+
 	protected void startActivity (Class<?> cls , boolean bringToTop, Bundle b){
 		Intent intent = new Intent(getApplicationContext(), cls);
 		if(bringToTop){
@@ -549,16 +591,16 @@ abstract public class BaseActivity extends LocationActivity implements IRequestR
 		}
 		startActivity(intent);
 	}
-	
+
 	protected void makeToast(String str){
 		Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
 	}
 	protected void makeToast(int resId){
 		Toast.makeText(getApplicationContext(), resId, Toast.LENGTH_SHORT).show();
 	}
-	
+
 	public void showNetworkFailToast(){
 		Toast.makeText(this, R.string.error_network_request_fail, Toast.LENGTH_SHORT).show();
 	}
-	
+
 }
