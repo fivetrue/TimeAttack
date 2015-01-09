18bb1e5d2872fe9da6a64bfa0b4e0c3f51805c50
dudrpdjwls <dudrpdjwls@efecd4e3-bc03-0510-bdcf-e5a0e64816a0>
2015-01-09 17:46:22 +0000

    Setting정리, Map 플로팅 아이콘 변경.

--- TimeAttack/src/com/fivetrue/timeattack/activity/SettingActivity.java
+++ TimeAttack/src/com/fivetrue/timeattack/activity/SettingActivity.java
@@ -1,13 +1,20 @@
 package com.fivetrue.timeattack.activity;
 
 import com.fivetrue.timeattack.R;
+import com.fivetrue.timeattack.preference.MapPreferenceManager;
+import com.google.android.gms.maps.GoogleMap;
 
 import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class SettingActivity extends BaseActivity {
 	
@@ -15,17 +22,30 @@ public class SettingActivity extends BaseActivity {
 	
 	static public class ViewHolder {
 		public ScrollView scrollView = null;
-		public ViewGroup layoutAccountSetting = null;
 		public ViewGroup layoutMapSetting = null;
+		public ViewGroup layoutMapSettingDetail = null;
+		public TextView tvMapSettingType = null;
+		public TextView tvMapSettingInteract = null;
+		public RadioGroup rgMapSetting = null;
+		public CheckBox cbMapSettingRotation = null;
+		public CheckBox cbMapSettingZoomButton = null;
+		
+		
+		public ViewGroup layoutAccountSetting = null;
 	}
 	
 	private ViewHolder mViewHolder = new ViewHolder();
+	
+	private MapPreferenceManager mMapPref = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater) {
 		// TODO Auto-generated method stub
 		
+		initModels();
 		mContentView = initViews(inflater);
+		initViewData();
+		
 		return mContentView;
 	}
 	
@@ -44,14 +64,101 @@ public class SettingActivity extends BaseActivity {
 		
 		mViewHolder.scrollView = (ScrollView) view.findViewById(R.id.scroll_setting);
 		mViewHolder.layoutMapSetting = (ViewGroup) view.findViewById(R.id.layout_map_setting);
-		mViewHolder.layoutAccountSetting = (ViewGroup) view.findViewById(R.id.layout_account_setting);
+		mViewHolder.layoutMapSettingDetail  = (ViewGroup) view.findViewById(R.id.layout_map_setting_detail);
+		mViewHolder.tvMapSettingType = (TextView)  view.findViewById(R.id.tv_setting_map_type);
+		mViewHolder.tvMapSettingInteract = (TextView)  view.findViewById(R.id.tv_setting_map_interact);
+		
+		mViewHolder.cbMapSettingRotation = (CheckBox) view.findViewById(R.id.cb_setting_map_rotation);
+		mViewHolder.cbMapSettingZoomButton = (CheckBox) view.findViewById(R.id.cb_setting_map_zoom_button);
+		mViewHolder.rgMapSetting = (RadioGroup) view.findViewById(R.id.rg_setting_map);
 		
 		mViewHolder.layoutMapSetting.setBackground(getResources().getDrawable(R.color.setting_primary_color));
+		mViewHolder.tvMapSettingType.setBackground(getResources().getDrawable(R.color.setting_primary_soft_color));
+		mViewHolder.tvMapSettingInteract.setBackground(getResources().getDrawable(R.color.setting_primary_soft_color));
+		
+		mViewHolder.layoutAccountSetting = (ViewGroup) view.findViewById(R.id.layout_account_setting);
 		mViewHolder.layoutAccountSetting.setBackground(getResources().getDrawable(R.color.setting_primary_color));
 		
 		
 		view.setBackground(getResources().getDrawable(R.color.setting_primary_light_color));
 		return view;
+	}
+	
+	private void initViewData(){
+		switch(mMapPref.getMapType()){
+		case GoogleMap.MAP_TYPE_NORMAL :
+			mViewHolder.rgMapSetting.check(R.id.rb_setting_map_normal);
+			break;
+			
+		case GoogleMap.MAP_TYPE_SATELLITE :
+			mViewHolder.rgMapSetting.check(R.id.rb_setting_map_satellite);
+			break;
+			
+		case GoogleMap.MAP_TYPE_TERRAIN :
+			mViewHolder.rgMapSetting.check(R.id.rb_setting_map_terrain);
+			break;
+			
+		case GoogleMap.MAP_TYPE_HYBRID :
+			mViewHolder.rgMapSetting.check(R.id.rb_setting_map_hybrid);
+			break;
+		}
+		
+		mViewHolder.rgMapSetting.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// TODO Auto-generated method stub
+				if(group != null){
+					switch(checkedId){
+					case R.id.rb_setting_map_normal : 
+						mMapPref.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+						break;
+					case R.id.rb_setting_map_satellite :
+						mMapPref.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+						break;
+						
+					case R.id.rb_setting_map_terrain :
+						mMapPref.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+						break;
+						
+					case R.id.rb_setting_map_hybrid :
+						mMapPref.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+						break;
+					}
+				}
+			}
+		});
+		
+		mViewHolder.cbMapSettingRotation.setChecked(mMapPref.isRotateEnable());
+		mViewHolder.cbMapSettingRotation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if(buttonView != null){
+					buttonView.setChecked(isChecked);
+					mMapPref.setRotateEnable(isChecked);
+				}
+			}
+		});
+		
+		mViewHolder.cbMapSettingZoomButton.setChecked(mMapPref.isZoomButtonEnable());
+		mViewHolder.cbMapSettingZoomButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if(buttonView != null){
+					buttonView.setChecked(isChecked);
+					mMapPref.setZoomButtonEnable(isChecked);
+				}
+			}
+		});
+		
+	}
+	
+	private void initModels(){
+		mMapPref = MapPreferenceManager.newInstance(this);
 	}
 	
 
